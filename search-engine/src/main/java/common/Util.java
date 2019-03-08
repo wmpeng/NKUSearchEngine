90e65ef0e60e4c2e824fb85302326dd4bb4b7172
wmpeng <wmpeng@outlook.com>
2019-03-08 17:17:00 +0800

    local config and secert config

--- search-engine/src/main/java/common/Util.java
+++ search-engine/src/main/java/common/Util.java
@@ -5,41 +5,48 @@ import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Util {
-    private static JSONObject jsonObject1 = null;
-    private static JSONObject jsonObject2 = null;
+    private static Map<String, Object> jsonMap = null;
     private static String configPath = "src\\main\\resources\\config\\config.json";
+    private static String localConfigPath = "src\\main\\resources\\config\\local-config.json";
     private static String secretConfigPath = "src\\main\\resources\\config\\secret-config.json";
 
+    private static Map<String, Object> readJson(String path){
+        String content;
+        File file = new File(path);
+        try {
+            content = FileUtils.readFileToString(file, "UTF-8");
+        } catch (IOException e) {
+            content="";
+        }
+        JSONObject jsonObject = new JSONObject(content);
+        return jsonObject.toMap();
+    }
+
     public static Object getConfig(String name) {
-        if (jsonObject1 == null){
-            try {
-                String content;
-                File file = new File(configPath);
-                content = FileUtils.readFileToString(file, "UTF-8");
-                jsonObject1 = new JSONObject(content);
-                file = new File(secretConfigPath);
-                content = FileUtils.readFileToString(file, "UTF-8");
-                jsonObject2 = new JSONObject(content);
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.exit(1);
-            }
+        if (jsonMap == null){
+            jsonMap = new HashMap<>();
+            jsonMap.putAll(readJson(configPath));
+            jsonMap.putAll(readJson(localConfigPath));
+            jsonMap.putAll(readJson(secretConfigPath));
         }
         try{
-            return jsonObject1.get(name);
+            return jsonMap.get(name);
         }catch (org.json.JSONException e){
-            return jsonObject2.get(name);
+            return null;
         }
     }
 
     public static void main(String[] argv) throws IOException {
-        System.out.println("document: " + getConfig("path.document"));
-        System.out.println("index: " + getConfig("path.index"));
-        double d = (double) getConfig("double");
-        System.out.println("double: " + getConfig("double"));
-        int i = (int) getConfig("int");
-        System.out.println("int: " + i);
+//        System.out.println("document: " + getConfig("path.document"));
+//        System.out.println("index: " + getConfig("path.index"));
+//        double d = (double) getConfig("double");
+//        System.out.println("double: " + getConfig("double"));
+//        int i = (int) getConfig("int");
+//        System.out.println("int: " + i);
+        getConfig("redis.port");
     }
 }
